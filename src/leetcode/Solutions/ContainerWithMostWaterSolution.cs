d73a5a630770aa258639e50e85ff7d139db03cb0
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-03-31 21:53:16 -0700

    Add container with most water solution

---
+++ src/leetcode/Solutions/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,25 @@
+﻿public class ContainerWithMostWaterSolution
+{
+    public int MaxArea(int[] height)
+    {
+        var left = 0;
+        var right = height.Length - 1;
+        var result = 0;
+
+        while (left < right)
+        {
+            var water = Math.Min(height[left], height[right]) * (right - left);
+
+            if (water > result)
+                result = water;
+
+            if (height[left] > height[right])
+                right--;
+            else
+                left++;
+
+        }
+
+        return result;
+    }
+}
