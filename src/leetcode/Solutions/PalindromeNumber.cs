7d6edfc29672da26aaa294f344f181f7d7a0832d
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-03-31 21:54:41 -0700

    Add Palindrome Number

---
+++ src/leetcode/Solutions/PalindromeNumber.cs
@@ -0,0 +1,21 @@
+﻿public class PalindromeNumber
+{
+    public bool IsPalindrome(int x)
+    {
+        var s = Convert.ToString(x);
+        var left = 0;
+        var right = s.Length - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+
+    }
+}
