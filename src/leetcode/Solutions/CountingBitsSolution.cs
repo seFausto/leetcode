d5ed6584f8f0b79080c6b7ceceb585006464822b
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-03-31 21:56:04 -0700

    Add Couting bits solution

---
+++ src/leetcode/Solutions/CountingBitsSolution.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+public class CoutingBitSolution
+{
+    int NumberBase = 2;
+    public int[] CountBits(int n)
+    {
+        var ans = new int[n + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            var currentCount = 0;
+            var bits = Convert.ToString(i, NumberBase);
+
+            //count all the 1
+            foreach (var bit in bits)
+            {
+                if (bit == '1')
+                    currentCount++;
+            }
+
+            ans[i] = currentCount;
+        }
+
+        return ans;
+    }
+}
