8cc6943046063fe58476b7667225474097494bbf
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-03-31 21:44:50 -0700

    Add Valid Parentheses solution

---
+++ src/leetcode/Solutions/ValidParenthesesSolution.cs
@@ -0,0 +1,48 @@
+﻿public class ValidParenthesesSolution
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach (char c in s)
+        {
+            if (IsOpening(c))
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                //if it's the first char, then false
+                if (stack.Count == 0)
+                    return false;
+
+                if (CanClose(stack.Peek(), c))
+                    stack.Pop();
+                else
+                    return false;
+
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    private bool CanClose(char s, char c)
+    {
+        if (s == '{')
+            return c == '}';
+
+        if (s == '[')
+            return c == ']';
+
+        if (s == '(')
+            return c == ')';
+
+        return false;
+    }
+
+    private bool IsOpening(char c)
+    {
+        return c == '(' || c == '[' || c == '{';
+    }
+}
